c174e84e85a0812c3fec9f5e2fbebcfe7a0e4ce9
thomas <thangpd.slz@gmail.com>
2020-11-01 02:42:02 +0700

    deleete rev all in one plugin

--- wp-content/plugins/all-in-one-favicon/appsumo.php
+++
@@ -1,7 +0,0 @@
-<?php add_thickbox(); ?>
-<div class="wrap">
-        <script>
-                window.open('https://appsumo.com/tools/wordpress/?utm_source=sumo&utm_medium=wp-widget&utm_campaign=all-in-one-favicon');
-                window.history.back();
-        </script>
-</div>
