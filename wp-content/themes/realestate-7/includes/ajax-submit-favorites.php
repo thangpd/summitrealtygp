95672d5c56354ca66d93a467e9e77aa3ec639271
thomas <thangpd.slz@gmail.com>
2020-11-16 16:10:30 +0700

    update ajax mail favorite

--- wp-content/themes/realestate-7/includes/ajax-submit-favorites.php
+++ wp-content/themes/realestate-7/includes/ajax-submit-favorites.php
@@ -5,31 +5,35 @@
  * @package WP Pro Real Estate 7
  * @subpackage Include
  */
-
-$email=$_POST['email'];
-$name=$_POST['name'];
-$message=$_POST['message'];
-$youremail=$_POST['ctyouremail'];
-$subject=$_POST['ctsubject'];
-$ctproperty=$_POST['ctproperty'];
-$ctphone=$_POST['ctphone'];
-
-$isValidate = true;
-
-if($isValidate == true){
-	$to = "$youremail";
+require_once("../../../../wp-load.php");
+$email                                     = $_POST['email'];
+$name                                      = $_POST['name'];
+$message                                   = $_POST['message'];
+$youremail                                 = $_POST['ctyouremail'];
+$subject                                   = $_POST['ctsubject'];
+$ctproperty                                = $_POST['ctproperty'];
+$ctphone                                   = $_POST['ctphone'];
+$ct_listing_agent_contact_cc_email_address = '';
+$isValidate                                = true;
+if ( $isValidate ) {
+	ob_start();
+	$to      = "$youremail";
 	$subject = "$subject";
-	$msg = "$message" . "\n\n" .
-	"Phone: $ctphone" . "\n" .
-	"Email: $email" . "\n" .
-	"Properties: $ctproperty" . "\n" .
-	$headers = "From: $name <$email>" . "\r\n" .
-		"Reply-To: $email" . "\r\n" .
-		"CC: $ct_listing_agent_contact_cc_email_address" . "\r\n" .
-		"X-Mailer: PHP/" . phpversion();
-	mail ($to, $subject, $msg, "From: $name <$email>");
+	$msg     = "$message" . "\n\n" .
+	           "Phone: $ctphone" . "\n" .
+	           "Email: $email" . "\n" .
+	           "Properties: $ctproperty" . "\n" .
+	           $headers = "From: $name <$email>" . "\r\n" .
+	                      "Reply-To: $email" . "\r\n" .
+	                      "CC: $ct_listing_agent_contact_cc_email_address" . "\r\n" .
+	                      "X-Mailer: PHP/" . phpversion();
+	wp_mail( $to, $subject, $msg, "From: $name <$email>" );
+
+	$smtp_debug = ob_get_clean();
+	echo $smtp_debug;
 	echo "true";
 } else {
-	echo '{"jsonValidateReturn":'.json_encode($arrayError).'}';
+	$arrayError = '';
+	echo '{"jsonValidateReturn":' . json_encode( $arrayError ) . '}';
 }
 ?>
